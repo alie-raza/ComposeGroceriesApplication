a95945a7b20ef71c84559e82e3613ba2a7f4beca
Ali Raza <Ali@Verokes-MacBook-Pro.local>
2025-10-01 12:57:08 +0500

    Search bar is implemented on Home screen

--- Grocesry App/app/src/main/java/com/alidev/groceriesapplication/presentation/screen/home/HomeScreen.kt
+++ Grocesry App/app/src/main/java/com/alidev/groceriesapplication/presentation/screen/home/HomeScreen.kt
@@ -1,25 +1,36 @@
 package com.alidev.groceriesapplication.presentation.screen.home
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
-//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.alidev.groceriesapplication.R
+import com.alidev.groceriesapplication.navigation.screen.Screen
+import com.alidev.groceriesapplication.presentation.component.SearchViewBar
+import com.alidev.groceriesapplication.ui.theme.DIMENS_24dp
 import com.alidev.groceriesapplication.ui.theme.GilroyFontFamily
 import com.alidev.groceriesapplication.ui.theme.GrayThirdTextColor
 import com.alidev.groceriesapplication.ui.theme.TEXT_SIZE_12sp
-
 
 
 @Composable
@@ -41,13 +52,13 @@ fun HomeScreen(
             HeaderLocationHome()
 
 
-//            SearchViewBar(
-//                hint = stringResource(id = R.string.search_store),
-//                query = searchQuery,
-//                onValueChange = {
-//                    if (it.isNotEmpty()) navController.navigate(Screen.Search.route)
-//                }
-//            )
+            SearchViewBar(
+                hint = stringResource(id = R.string.search_store),
+                query = "as",
+                onValueChange = {
+                    if (it.isNotEmpty()) navController.navigate(Screen.Search.route)
+                }
+            )
 //
 //            SliderBanner()
 
@@ -82,40 +93,33 @@ fun HeaderLocationHome(
         modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = stringResource(R.string.sample_country),
-            fontFamily = GilroyFontFamily,
-            fontWeight = FontWeight.SemiBold,
-            fontSize = TEXT_SIZE_12sp,
-            color = GrayThirdTextColor
+        Spacer(modifier = Modifier.height(DIMENS_24dp))
+
+        Icon(
+            modifier = Modifier.size(DIMENS_24dp)
+                .align(Alignment.CenterHorizontally),
+            painter = painterResource(id = R.drawable.ic_nectar),
+            contentDescription = stringResource(id = R.string.logo_app),
+            tint = Color.Unspecified
         )
-//        Spacer(modifier = height(DIMENS_24dp))
-//
-//        Icon(
-//            modifier = size(DIMENS_24dp)
-//                .align(Alignment.CenterHorizontally),
-//            painter = painterResource(id = R.drawable.ic_nectar),
-//            contentDescription = stringResource(id = R.string.logo_app),
-//            tint = Color.Unspecified
-//        )
-//
-//        Spacer(modifier = height(DIMENS_8dp))
-//
-//        Row {
-//            Icon(
-//                imageVector = Icons.Default.LocationOn,
-//                contentDescription = stringResource(R.string.image_location),
-//                tint = GrayThirdTextColor
-//            )
-//            Text(
-//                modifier = Modifier.align(Alignment.CenterVertically),
-//                text = stringResource(R.string.sample_country),
-//                fontFamily = GilroyFontFamily,
-//                fontWeight = FontWeight.SemiBold,
-//                fontSize = TEXT_SIZE_12sp,
-//                color = GrayThirdTextColor
-//            )
-//        }
+
+        Spacer(modifier = Modifier.height(DIMENS_24dp))
+
+        Row {
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = stringResource(R.string.image_location),
+                tint = GrayThirdTextColor
+            )
+            Text(
+                modifier = Modifier.align(Alignment.CenterVertically),
+                text = stringResource(R.string.sample_country),
+                fontFamily = GilroyFontFamily,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = TEXT_SIZE_12sp,
+                color = GrayThirdTextColor
+            )
+        }
     }
 }
 
